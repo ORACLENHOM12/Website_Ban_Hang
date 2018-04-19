84342cd0f406174374b1139782f22dcb3b3f1cf1
Tuyenttk1997 <38147315+Tuyenttk1997@users.noreply.github.com>
2018-04-19 09:40:33 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,13 @@
+	<div class="menu" >
+		<ul>
+			<li><a href="index.php">TRANG CHỦ</a></li>
+			<li><a href="index.php">SẢN PHẨM</a></li>
+			<li><a href="#">HƯỚNG DẪN</a></li>
+			<li><a href="#">LIÊN HỆ</a></li>
+			<form action="index.php" method="post" enctype="multipart/form-data">
+			<div class="searchform">
+			<input class="tk" type="text" name="txtsearch" style="width: 250px;height: 30px" ><input class="se"  type="submit" name="search" value="Search">
+			</div>
+			</form>
+		</ul>
+	</div>

50ea31f6dfea9a553b096256a24adb61ecac1dca
kimthoa25091997 <37947159+kimthoa25091997@users.noreply.github.com>
2018-04-20 22:17:07 +0700

    sua loi search.php

--- search.php
+++ search.php
@@ -1,13 +1,10 @@
 <?php 
 if(isset($_POST['search'])){
 	$search=$_POST['txtsearch'];
-	$sql_search="select * from SANPHAM where TENSP LIKE %$search%";
-$sql_query = 'SELECT COUNT(*) AS NUMBER_OF_ROWS FROM (' . $sql_search . ')';
-$stmt= oci_parse($conn, $sql_query);
-oci_define_by_name($stmt, 'NUMBER_OF_ROWS', $number_of_rows);
-oci_execute($stmt);
-oci_fetch($stmt);
-echo $number_of_rows;
+	$sql_search="SELECT * from SANPHAM WHERE  upper(TENSP) like upper('%$search%')";
+$run=oci_parse($conn,$sql_search);
+oci_execute($run);
+
 
 
 }
@@ -17,12 +14,12 @@ echo $number_of_rows;
 			<p align="left" style="background: #ee145b;padding: 3px;color: aliceblue;font-size: 19px;font-family: Segoe, Segoe UI, DejaVu Sans, Trebuchet MS, Verdana,' sans-serif';margin-left: 20px;" >Sản phẩm tìm thấy</p>
 			<div class="sanpham">
 			<?php
-				if($number_of_rows==0){?>
+				/*if($number_of_rows==0){?>
 				<p align="center">Không tìm thấy sản phẩm nào</p>
 						
 			<?php	
 				}
-				else{						
+				else{	*/					
 				?>
 				<ul>
 				<?php
@@ -37,7 +34,6 @@ echo $number_of_rows;
 						<p style="color: red"><?php echo number_format($dong['GIA'])?> VND</p>
 					</a></li>
 						<?php
-					}
 				}
 				?>
 				</ul>
